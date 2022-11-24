a9b6369380ea90c754fbc312528d42a80a7ff539
Zephyrus011 <ritvikgupta011@gmail.com>
2022-11-24 12:01:36 -0500

    fixed errors

--- pages/projects/millburnai.js
+++ pages/projects/millburnai.js
@@ -1,4 +1,4 @@
-import Layout from '../../components/Layout';
+import Layout from '../../components/layout';
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
 
