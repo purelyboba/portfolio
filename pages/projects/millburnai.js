896749d67aa47e76f82aeb17eceea0aff6e512b0
Zephyrus011 <ritvikgupta011@gmail.com>
2022-11-26 12:28:08 -0500

    updated millburnai project, favicon

--- pages/projects/millburnai.js
+++ pages/projects/millburnai.js
@@ -15,6 +15,10 @@ export default function MillburnAI() {
                     In my freshman year of High School, I joined the robotics program.
                     It was made up of 4 VEX Robotics teams, and 1 VEX AI team. I joined the 
                     VEX AI team, with the goal of competing in the new VEX AI competition.
+                    Our objective was to make a robot to play that years VEX challenge (Tipping Point)
+                    fully autonomously. 
+                    More information can be found on the <a href="https://millburnai.org" target="_blank">website</a> that I built for the team,
+                    as well as our <a href="https://www.youtube.com/@millburnrobotics174" target="_blank">YouTube channel</a>.
                 </p>
             </main>
         </Layout>
