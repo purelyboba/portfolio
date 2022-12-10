9e5e5cac2f0ad402f44283ddc5fd28ecb0c8e13d
Ritvik Gupta <86858242+R1tzG@users.noreply.github.com>
2022-12-10 14:58:11 -0500

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -20,7 +20,7 @@ export default function Layout({ children, home }) {
               <Link className={utilStyles.navTitle} href="/"><h1 className={utilStyles.heading2Xl}>{name}</h1></Link>
               <ul className={styles.socials}>
                 <li><a href="https://github.com/R1tzG" target="_blank">github</a></li>
-                <li><a href="https://twitter.com/ritvikgupta011" target="_blank">twitter</a></li>
+                <li><a href="https://twitter.com/_ritvikg" target="_blank">twitter</a></li>
                 <li><a href="https://www.linkedin.com/in/ritvikgupta11/" target="_blank">linkedin</a></li>
               </ul>
             </div>
