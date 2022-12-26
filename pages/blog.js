86e249038635927bd742cad6d54421f4316e4531
Zephyrus011 <ritvikgupta011@gmail.com>
2022-12-26 13:54:23 -0500

    unfinished search, link styling

--- pages/blog.js
+++ pages/blog.js
@@ -4,7 +4,6 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
-import SearchBar from '../components/searchBar';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -16,12 +15,32 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+  function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("blogInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("blogList");
+    li = ul.getElementsByTagName("li");
+  
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  
+    console.log(input);
+  }
+  
   return (
     <Layout>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingX1}>Blog</h2>
-        {/* <SearchBar allPostsData={allPostsData} /> */}
-        <ul className={utilStyles.list}>
+        <input type="text" id="blogInput" className={utilStyles.searchBar} onkeyup="search()" placeholder="Search..." />
+        <ul className={utilStyles.list} id="blogList">
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
