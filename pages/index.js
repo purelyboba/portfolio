a564ce98e8f8f7eff516bd60a4851ec28d6eb317
Ritvik Gupta <86858242+R1tzG@users.noreply.github.com>
2023-08-01 16:56:24 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <section className={utilStyles.headingMd}>
         <p>
-          Hi! I'm <strong>Ritvik</strong>, a student from New Jersey focusing on real-world robotics and machine learning applications. Apart from programming, I enjoy making music and watching motorsport.
+          Hi! I'm <strong>Ritvik</strong>, a student from New Jersey focusing on real-world robotics and machine learning applications. Apart from programming, I enjoy making music and watching motorsports.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
