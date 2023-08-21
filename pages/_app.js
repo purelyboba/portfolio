83a47238d3e8e5a0aded0e1269c30d5e1da1ef9f
Ritvik Gupta <ritvikgupta011@gmail.com>
2023-08-21 16:35:54 -0400

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Script from 'next/script'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-X9HXHWR4MM"/>
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-X9HXHWR4MM');
+                `}
+            </Script>
+
+            <Component {...pageProps} />;
+        </>
+    )
 }
